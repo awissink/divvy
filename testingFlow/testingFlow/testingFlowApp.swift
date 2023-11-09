4fb7e621615bde4594e835a7ef76ebf289c3778f
Joyce Jiang <jj3115@barnard.edu>
2023-11-08 19:56:07 -0500

    new swiftui file

---
+++ testingFlow/testingFlow/testingFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  testingFlowApp.swift
+//  testingFlow
+//
+//  Created by joyce jiang on 11/8/23.
+//
+
+import SwiftUI
+
+@main
+struct testingFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
