b039400a724206e0821d249130156daeb55b50af
Grace Perrin <ghp2121@barnard.edu>
2023-11-11 01:47:36 -0500

    improving ui of signup page to match figma mockups

--- Divvy/Divvy/AuthScreen.swift
+++ Divvy/Divvy/AuthScreen.swift
@@ -16,23 +16,71 @@ struct AuthView: View {
     
     var body: some View {
     
-        
+        //VStack for all of the elements on the signup page
         VStack {
-            Text("Welcome to Divvy!")
-                .onAppear {
-                    print("hey there")
-                }
-                .font(.largeTitle)
+            
+            //Placeholder image for the swipe stack animation example
+            Image(.exampleStack)
+            
+            Spacer().frame(height:30)
+            
+            //Logo and tag line
+            Image(.logo)
+            Text("the new way to split the bill")
+                .font(.system(size:26))
                 .textFieldStyle(RoundedBorderTextFieldStyle())
-
-            TextField("Enter phone number", text: $phoneNumber)
-                .padding()
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-
-            Button("Verify Phone Number") {
-                PhoneVerificationViewModel().verifyPhoneNumber(phoneNumber)
+            
+            Spacer().frame(height: 30)
+            
+            //Text field and its description
+            Text("enter your number")
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.leading, 20)
+                .font(.system(size:16, weight:.medium))
+                .foregroundColor(Color(red: 0xE0 / 330.0, green: 0xE0 / 330.0, blue: 0xE0 / 330.0))
+            Spacer().frame(height: 5)
+            TextField("", text: $phoneNumber)
+                .frame(width: 326, height: 40)
+                .background(Color(red: 0xE0 / 240.0, green: 0xE0 / 240.0, blue: 0xE0 / 240.0))
+                .cornerRadius(10)
+                .textFieldStyle(PlainTextFieldStyle())
+            
+            Spacer().frame(height: 10)
+            
+            //Enter button which will send verification code
+            //Will also link with firebase to create a new account (TBI)
+            Button(action: {
+                print("sign up button tapped")
+            }) {
+                Text("enter")
+                    .foregroundColor(.white)
+                    .padding(.leading, 100)
+                    .padding(.trailing, 100)
+                    .padding(.top, 15)
+                    .padding(.bottom, 15)
+                    .background(Color(red: 0x3E / 255.0, green: 0x88 / 255.0, blue: 0x5B / 255.0))
+                    .cornerRadius(20)
+                    .font(.system(size:18, weight:.semibold))
             }
             .padding()
+            
+            //For users that already have an account, allows them to sign in instead of signing up
+            Button(action: {
+                //do action to bring them to a sign in page instead of the sign up page
+                print("sign in button tapped")
+            }) {
+                Text("already a user? login here")
+                    .offset(y: -10)
+                    .font(.system(size:16, weight:.medium))
+                    .foregroundColor(Color(red: 0xE0 / 330.0, green: 0xE0 / 330.0, blue: 0xE0 / 330.0))
+                    .underline()
+            }
+            
+            
+//            Button("Verify Phone Number") {
+//                PhoneVerificationViewModel().verifyPhoneNumber(phoneNumber)
+//
+//            }
 
             if let errorMessage = PhoneVerificationViewModel().errorMessage {
                 Text(errorMessage)
@@ -45,7 +93,9 @@ struct AuthView: View {
                 Text("Verification ID: \(verificationID)")
             }
         }
-        .padding()
+        //add padding and offset to align elements and push the whole VStack up on the screen
+            .offset(y: -50)
+            .padding()
 
         
     }
