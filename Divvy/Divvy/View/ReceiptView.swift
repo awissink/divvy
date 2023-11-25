73720b194df1bc8130537dc7a40152d06e0e15e8
Joyce Jiang <jj3115@barnard.edu>
2023-11-25 07:32:05 -0500

    added dropdown in receiptView but need to edit positioning

--- Divvy/Divvy/View/ReceiptView.swift
+++ Divvy/Divvy/View/ReceiptView.swift
@@ -20,7 +20,7 @@ struct ReceiptItem: Identifiable {
 
 // Updated ReceiptView with the ViewModel's functionality
 struct ReceiptView: View {
-    @State private var restaurantName: String = "Restaurant Name"
+    @State private var restaurantName: String = "Restaurant name"
     @State private var receiptItems: [ReceiptItem] = [
         ReceiptItem(name: "Burger", price: 9.99, quantity: 1),
         ReceiptItem(name: "Fries", price: 4.99, quantity: 2),
@@ -37,6 +37,8 @@ struct ReceiptView: View {
     var body: some View {
         VStack {
             HStack {
+                Image(systemName: "fork.knife")
+                    .foregroundColor(.secondary)
                 Spacer()
                 Text(restaurantName)
                     .font(.title)
@@ -47,7 +49,7 @@ struct ReceiptView: View {
                 .foregroundColor(.white)
                 .frame(width: 80, height: 40)
                 .background(Color.green)
-                .cornerRadius(10)
+                .cornerRadius(28)
                 .font(.system(size:18, weight:.semibold))
             }
             List {
@@ -87,9 +89,26 @@ struct ReceiptItemView: View {
     @Binding var receiptItem: ReceiptItem
     var isEditing: Bool
     
+//    var body: some View {
+//        // Define your receipt item view
+//        Text("\(receiptItem.name) - \(receiptItem.total, specifier: "%.2f")")
+//    }
+    
     var body: some View {
-        // Define your receipt item view
-        Text("\(receiptItem.name) - \(receiptItem.total, specifier: "%.2f")")
+        VStack {
+            HStack {
+                Text(receiptItem.name)
+                    .font(.headline)
+                
+                Spacer()
+                
+                Text("\(receiptItem.total, specifier: "%.2f")")
+                    .foregroundColor(.secondary)
+            }
+            customDropdownView()
+                .frame(maxWidth: .infinity, alignment: .leading)
+            .padding(.vertical, 8)
+        }
     }
 }
 
@@ -114,9 +133,91 @@ struct ReceiptEditableValue: View {
             }
         }
         .padding()
+        
     }
 }
 
+struct customDropdownView: View {
+
+    let drop: [DropItem] = [
+        DropItem(title: "joyce"),
+        DropItem(title: "izzy"),
+        DropItem(title: "grace"),
+        DropItem(title: "eki"),
+        DropItem(title: "amelia"),
+        DropItem(title: "hannah")
+        // Add more items as needed
+    ]
+    
+    @State var show = false
+    @State var name = "who had this item?"
+
+    
+    var body: some View {
+        
+        VStack {
+            ZStack{
+                ZStack{
+                    RoundedRectangle(cornerRadius: 10)
+                    ScrollView{
+                        VStack(spacing: 17){
+                            ForEach (drop) { item in
+                                Button {
+                                    withAnimation {
+                                        name = item.title
+                                        show.toggle()
+                                    }
+                                } label: { //list of names
+                                    Text(item.title).foregroundColor(.black)
+                                        .bold()
+                                        .font(.system(size: 14))
+                                    Spacer()
+                                }
+                            }
+                            .padding(.horizontal)
+                        }
+                        .frame (maxWidth: .infinity,alignment: .leading)
+                        .padding(.vertical,15)
+                    }
+                }
+                .frame (height: show ? 205 : 0)
+                .offset(y: show ? 190 : -25)
+                .foregroundColor(Color(red: 0.95, green: 0.95, blue: 0.95))
+                
+                
+                //who had item for dropdown
+                ZStack{
+                    RoundedRectangle(cornerRadius: 5).frame(height: 40)
+                        .foregroundColor(Color.white)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(Color.gray.opacity(0.5), lineWidth: 1)
+                        )
+                        .shadow(color: Color.gray.opacity(0.1), radius: 4, x: 0, y: 2) // Apply slight gray shadow
+                    HStack{
+                        Text(name).font(.subheadline)
+                        Spacer()
+                        Image(systemName: "chevron.right")
+                            .rotationEffect(.degrees(show ? -90 : 0))
+                            .font(.system(size: 12))
+                    }
+                    
+                    .padding(.horizontal)
+                    .foregroundColor(.black)
+                }
+                
+                .offset(y: -25)
+                .onTapGesture {
+                    withAnimation{
+                        show .toggle()
+                    }
+                }
+            }
+        }
+        .padding()
+        
+    }
+}
 
 #Preview {
     ReceiptView()
