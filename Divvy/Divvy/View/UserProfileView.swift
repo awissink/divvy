46b325c80eed7dbf562f72b4c40509ca4e798a1c
Eki <ekiuzams@gmail.com>
2023-11-30 20:34:33 -0500

    Adding new profile view

--- Divvy/Divvy/View/UserProfileView.swift
+++ Divvy/Divvy/View/UserProfileView.swift
@@ -29,23 +29,63 @@ struct UserProfileView: View {
         VStack {
             VStack(spacing: 20) {
                 // Profile picture or avatar placeholder
-                Image(systemName: "person.circle.fill") // Using a system icon as a placeholder
-                    .resizable()
-                    .aspectRatio(contentMode: .fill)
-                    .frame(width: 100, height: 100) // Set desired size for the profile picture
-                    .clipShape(Circle())
-                    .overlay(Circle().stroke(Color.gray, lineWidth: 1))
-                
-                // Email display
-                
-                    HStack {
+//                Image(systemName: "person.circle.fill") // Using a system icon as a placeholder
+//                    .resizable()
+//                    .aspectRatio(contentMode: .fill)
+//                    .frame(width: 100, height: 100) // Set desired size for the profile picture
+//                    .clipShape(Circle())
+//                    .overlay(Circle().stroke(Color.gray, lineWidth: 1))
+//                
+//                // Email display
+//                
+//                    HStack {
+//                        Text(email)
+//                    }
+//                    .frame (maxWidth: .infinity,alignment: .leading)
+//                    .padding()
+//                    .background(Color.white)
+//                    .cornerRadius(10)
+//                    .shadow(radius: 1)
+                ZStack {
+                    RoundedRectangle(cornerRadius: 20, style: .continuous)
+                        .fill(.linearGradient(colors: [Color(#colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1)),
+                                                       Color(#colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1))], startPoint: .topLeading, endPoint: .bottomTrailing))
+                    
+                    VStack(spacing: 10) {
+                        HStack {
+                            Image("EMVChip")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(height: 50)
+                            
+                            Spacer(minLength: 0)
+                            
+                            Image("CardLogo")
+                                .resizable()
+                                .renderingMode(.template)
+                                .aspectRatio(contentMode: .fit)
+                                .frame(height: 50)
+                        }
+                        
+                        HStack(spacing: 12) {
+                            Text("MM/YY")
+                            Spacer(minLength: 0)
+                            Text("CVV")
+                                .frame(width: 35)
+                        }
+                        .padding(.top,15)
+                        Spacer(minLength: 0)
                         Text(email)
+                            .font(.title3)
+                        
                     }
-                    .frame (maxWidth: .infinity,alignment: .leading)
-                    .padding()
-                    .background(Color.white)
-                    .cornerRadius(10)
-                    .shadow(radius: 1)
+                    .padding(20)
+                    .environment(\.colorScheme, .dark)
+                    .tint(.white)
+                }
+                .frame(height: 200)
+                .padding(.top, 35)
+                
                 }
             
             .padding()
@@ -55,28 +95,6 @@ struct UserProfileView: View {
                 EmptyView()
             }
             .hidden()
-
-            
-            
-//            // Logout button
-//            Button("Log Out") {
-//                do {
-//                    try Auth.auth().signOut()
-//                    userData.loggedIn = false
-//                    showingSignIn = true
-//                } catch let signOutError as NSError {
-//                    print("Error signing out: \(signOutError.localizedDescription)")
-//                }
-//            }
-//            .foregroundColor(.black)
-//            .frame(width: 284, height: 52)
-//
-//            .background(Color(red: 0.95, green: 0.95, blue: 0.95))
-//            .overlay{
-//                .cornerRadius(28)
-//            }
-//            .padding()
-//            Spacer()
             
             // Logout button
             Button("Log Out") {
